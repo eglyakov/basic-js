ca8722b61df5885648f040299112708a961a2adb
eglyakov <eglyakov@gmail.com>
2021-03-05 19:42:06 +0300

    feat: implement func countCats and dreamTeam

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+          if (arr[i][j] == '^^') newArr.push(arr[i][j]);
+      }
+  }
+
+  return newArr.length;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let arrShortName = []
+  for(let i = 0; i < members.length; i++) {
+    if (typeof members[i] != 'string') continue;
+
+    let firstLetter = members[i].trim().split('');
+    arrShortName.push(firstLetter[0].toUpperCase())
+    console.log(firstLetter[0])
+      
+  }
+
+  return arrShortName.sort().join('');
 };
